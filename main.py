471d037beb7848e62eeaed4b41a586b18f35574e
AlbinaHominyatich <hominyatichalbina@gmail.com>
2022-11-19 13:57:00 +0200

    вивели біткоїн

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ print(response.read())
 print('#################################################################################')
 #парсимо курс бітка
 import requests
+res_parse_list = []
 response = requests.get('https://coinmarketcap.com/')
 response_text = response.text
 response_parse = response_text.split('<span>')
@@ -13,7 +14,9 @@ for parse_elem_1 in response_parse:
         #print(parse_elem_1)
         for parse_elem_2 in parse_elem_1.split('</span>'):
             if parse_elem_2.startswith('$') and parse_elem_2[1].isdigit():
-                print(parse_elem_2)
+                res_parse_list.append(parse_elem_2)
+bitock = res_parse_list[0]
+print(bitock)
 
 
 
